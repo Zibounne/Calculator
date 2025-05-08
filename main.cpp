592d7ea396b777378b32d1ba4ca8a411727c45b6
Zibounne <tarik.dahhane.pro@gmail.com>
2025-05-08 13:04:03 +0200

    Adding: terminal fonctions in main.cpp

--- main.cpp
+++ main.cpp
@@ -2,7 +2,26 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Init Calculator Project";
+    std::cout
+        << "=== Calculator ===\n"
+        << "Type an expression or 'exit' to leave"
+        << std::endl;
+
+    while (true) {
+        std::cout << "\n> ";
+        std::string input;
+        std::getline(std::cin, input);
+
+        if (input == "exit") {
+            std::cout << "Bye!\n";
+            break;
+        }
+
+        std::cout
+            << "you entered: "
+            << input
+            << "\n";
+    }
 
     return 0;
 }
